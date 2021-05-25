c1fb0a9042aa90777117741bcaac7ab2060844bc
David <davidcobbina47@gmail.com>
2021-05-25 09:22:52 +0200

    encapsulated aboutSection web and mobile into one responsive section

--- lib/presentation/pages/home/sections/about_section/about_section.dart
+++ lib/presentation/pages/home/sections/about_section/about_section.dart
@@ -1,16 +1,155 @@
-import 'package:flutter/cupertino.dart';
-import 'package:responsive_builder/responsive_builder.dart';
+import 'package:amor/presentation/layout/adaptive.dart';
+import 'package:amor/presentation/widgets/circle.dart';
+import 'package:amor/presentation/widgets/section_title.dart';
+import 'package:amor/presentation/widgets/spaces.dart';
+import 'package:amor/presentation/widgets/sub_section_title.dart';
+import 'package:amor/values/values.dart';
+import 'package:flutter/material.dart';
+import 'package:layout/layout.dart';
 
-import 'about_section_mobile.dart';
-import 'about_section_web.dart';
+const double kSpacing16 = Sizes.HEIGHT_16;
 
-class AboutSection extends StatelessWidget {
+class AboutSection extends StatefulWidget {
+  @override
+  _AboutSectionState createState() => _AboutSectionState();
+}
+
+class _AboutSectionState extends State<AboutSection> {
+  GlobalKey aboutMeContainerKey = GlobalKey();
+  GlobalKey sectionTitleKey = GlobalKey();
+  double sectionTitleOffset = 0.0;
+  double aboutMeContainerOffset = 0.0;
+
+  @override
+  void initState() {
+    if (mounted) {
+      WidgetsBinding.instance?.addPostFrameCallback((_) {
+        _getHeightOfAboutContainer();
+      });
+    }
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(AboutSection oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (mounted) {
+      WidgetsBinding.instance?.addPostFrameCallback((_) {
+        _getHeightOfAboutContainer();
+      });
+    }
+  }
+
+  _getHeightOfAboutContainer() {
+    final RenderBox sectionTitleRenderBox =
+        sectionTitleKey.currentContext?.findRenderObject() as RenderBox;
+    final RenderBox aboutContainerRenderBox =
+        aboutMeContainerKey.currentContext?.findRenderObject() as RenderBox;
+
+    final sectionTitleHeight = sectionTitleRenderBox.size.height;
+    final aboutContainerHeight = aboutContainerRenderBox.size.height;
+    setState(() {
+      sectionTitleOffset = sectionTitleHeight;
+      aboutMeContainerOffset = aboutContainerHeight;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ScreenTypeLayout(
-      mobile: AboutSectionMobile(),
-      tablet: AboutSectionWeb(),
-      desktop: AboutSectionWeb(),
+    TextTheme textTheme = Theme.of(context).textTheme;
+    double screenWidth = widthOfScreen(context);
+    TextStyle? aboutMeTextStyle = textTheme.bodyText1?.copyWith(
+      fontSize: Sizes.TEXT_SIZE_18,
+      color: AppColors.accentColor,
+      fontWeight: FontWeight.w300,
+      letterSpacing: 0.1,
+    );
+    double containerPadding = context.layout.value(
+      xs: Sizes.PADDING_30,
+      sm: Sizes.PADDING_40,
+      md: Sizes.PADDING_40,
+      lg: Sizes.PADDING_40,
+      xl: Sizes.PADDING_40,
+    );
+
+    return Container(
+      child: Stack(
+        children: [
+          CustomPaint(
+            painter: Circle(
+              offset: Offset(
+                screenWidth * 0.875,
+                sectionTitleOffset + kSpacing16 + (aboutMeContainerOffset / 2),
+              ),
+              radius: (aboutMeContainerOffset / 2) - kSpacing16,
+              color: AppColors.accentColor100,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SectionTitle(
+                key: sectionTitleKey,
+                title: StringConst.ABOUT_ME,
+              ),
+              SizedBox(height: kSpacing16),
+              Container(
+                key: aboutMeContainerKey,
+                width: screenWidth * 0.85,
+                padding: EdgeInsets.all(containerPadding),
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: AppRadius.aboutRadius,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SubSectionTitle(
+                      title: StringConst.PROFESSIONAL,
+                      subtitle: StringConst.SUMMARY_TITLE,
+                    ),
+                    SpaceH30(),
+                    SizedBox(
+                      width: screenWidth * 0.8,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SelectableText(
+                            StringConst.ABOUT_ME_SUMMARY_1,
+                            style: aboutMeTextStyle,
+                          ),
+                          SpaceH8(),
+                          SelectableText(
+                            StringConst.ABOUT_ME_SUMMARY_2,
+                            style: aboutMeTextStyle,
+                          ),
+                          SpaceH8(),
+                          SelectableText(
+                            StringConst.ABOUT_ME_SUMMARY_3,
+                            style: aboutMeTextStyle,
+                          ),
+                          SpaceH8(),
+                          SelectableText(
+                            StringConst.ABOUT_ME_SUMMARY_4,
+                            style: aboutMeTextStyle,
+                          ),
+                          SpaceH8(),
+                          SelectableText(
+                            StringConst.ABOUT_ME_SUMMARY_5,
+                            style: aboutMeTextStyle,
+                          ),
+                          SpaceH8(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }

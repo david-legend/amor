3b62cafd585e80fe28ccda2fad7bd8717617ce10
David <dcobbina@bridgeinnovation.io>
2021-06-01 11:38:33 +0000

    merged porfolio into one section

--- lib/presentation/pages/home/sections/portfolio_section/portfolio_section_mobile.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class PortfolioSectionMobile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
